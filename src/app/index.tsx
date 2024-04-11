ba85306065dcfa21688e4c7c61ed342a41399aa6
Eduardo Sousa <eduardobrunoit@gmail.com>
2024-04-10 22:22:34 -0400

    Feat: add function to handle category

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import { CategoryButton } from '@/components/CategoryButton';
 import { Header } from '@/components/Header';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
+
+import { CATEGORIES } from '@/utils/data/products';
 
 export default function Home() {
+  const [category, setCategory] = useState<string>(CATEGORIES[0]);
+
+  function handleCategoryChange(newCategory: string) {
+    setCategory(newCategory);
+  }
+
   return (
     <View className="pt-8">
       <Header title="Faça seu pedido" cartQuantity={3} />
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategoryChange(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
