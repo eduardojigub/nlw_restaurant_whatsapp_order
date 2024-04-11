4ecd1091b77d9fcf87e6496b41f2b8c7b5c3814c
Eduardo Sousa <eduardobrunoit@gmail.com>
2024-04-10 22:03:44 -0400

    Feat: add icon shopping bag and state on header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 export default function Home() {
   return (
     <View className="pt-8">
-      <Header title="Faça seu pedido" />
+      <Header title="Faça seu pedido" cartQuantity={3} />
     </View>
   );
 }
